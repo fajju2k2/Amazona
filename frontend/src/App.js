5e5267aa8475c885c025439a4fbbf13257f47716
fajju2k2 <64869093+fajju2k2@users.noreply.github.com>
2022-11-26 03:56:14 +0500

    First

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Feroze Dental Supplies</a>
+      </header>
+      <main>Product List</main>
+    </div>
+  );
+}
+
+export default App;

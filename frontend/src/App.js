a3f23e49495532d6db35180a354dec48ea00d719
fajju2k2 <64869093+fajju2k2@users.noreply.github.com>
2022-11-27 04:01:39 +0500

    3rd upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Feroze Dental Supplies</a>
       </header>
-      <main>Product List</main>
+      <main>
+        <h1>Featured products</h1>
+        {data.products.map((product) => (
+          <div>
+            <img src={product.Image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
